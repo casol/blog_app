cc363eb2190964bdafb965fc9159f26ab3cd8053
casol <casoltys@gmail.com>
2017-02-14 20:17:24 +0100

    Adding tagging functionality with 'django-taggit'.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 
 class PostListView(ListView):
@@ -15,8 +16,14 @@ class PostListView(ListView):
 
 # View which retrieving all the post with the published status
 # using published manager created in models.
-def post_list(request, category=None):
+def post_list(request, tag_slug=None, category=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -28,7 +35,8 @@ def post_list(request, category=None):
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'page': page,
-                                                   'posts': posts})
+                                                   'posts': posts,
+                                                   'tag': tag})
 
 # Display a single post and
 def post_detail(request, year, month, day, post):
